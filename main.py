47ff40993385e7c0577b33cfb70039b6b4762805
fathemancre <ymatcheswala@gmail.com>
2016-04-13 16:18:11 -0400

    adding base.html for template inheritnce and adding inheriting code added to templates

--- main.py
+++ main.py
@@ -18,26 +18,6 @@ import webapp2
 import os.path
 import jinja2
 
-form_html="""
-
-"""
-
-hidden_html = """
-<input type="hidden" name="food" value="%s">
-"""
-
-item_html = """
-<li>%s</li>
-"""
-
-shopping_list_html = """
-<br>
-<br>
-<h2>Shopping List</h2>
-<ul>
-%s
-</ul>
-"""
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
@@ -56,36 +36,9 @@ class Handler(webapp2.RequestHandler):
 
 class MainPage(Handler):    
     def get(self):
-
-        n=self.request.get("n")
-
-        if n:
-            n=int(n)
-
-        self.render("fizzbuzz.html", n=n)
+        items = self.request.get_all("food")
+        self.render("shopping_list.html", items=items)
         
-
-##        output = form_html
-##        output_hidden = ""
-##        
-##        items = self.request.get_all("food")
-##        if items:
-## #           output_hidden = ""
-##            output_items = ""
-##            for item in items:
-##               output_hidden += hidden_html % item
-##               output_items += item_html % item
-##
-##            output_shopping = shopping_list_html % output_items
-##            output += output_shopping
-##
-##        output = output % output_hidden
-
- #       self.write(output)
-
-#class MainHandler(Handler):
-#    def get(self):
-#        self.write(form_html)
 
 app = webapp2.WSGIApplication([
     ('/', MainPage)
